2126f10579d88ec4f90ddb1c7f100db46e6c7b35
the_shivers <rossbrian120@gmail.com>
2023-12-04 20:39:07 -0600

    Day 6, 2022

--- 2022/day5/solution.py
+++ 2022/day5/solution.py
@@ -26,17 +26,14 @@ def adjust_stacks2(stacks, line):
     stacks[nums[1]] = stacks[nums[1]][0:len(stacks[nums[1]]) - nums[0]]
     stacks[nums[2]] += in_transit
     
-def print_answer(stacks):
-    print("".join([stacks[i][-1] for i in range(1, 10)]))
-    
 # PT 1
 stacks, commands = get_initial_stacks(lines)
 for command in commands:
     adjust_stacks(stacks, command)
-print_answer(stacks) # FRDSQRRCD
+print("".join([stacks[i][-1] for i in range(1, 10)])) # FRDSQRRCD
 
 # PT 2
 stacks, commands = get_initial_stacks(lines)
 for command in commands:
     adjust_stacks2(stacks, command)
-print_answer(stacks) # HRFTQVWNN
+print("".join([stacks[i][-1] for i in range(1, 10)])) # HRFTQVWNN
---
+++ 2022/day6/solution.py
@@ -0,0 +1,20 @@
+# input_txt = 'advent_of_code/2022/day6/example.txt'
+input_txt = 'advent_of_code/2022/day6/input.txt'
+with open(input_txt) as file:
+    signal = file.readline().strip()
+
+for i, char in enumerate(signal):
+    if i <= 2:
+        continue
+    if len(set(signal[i-4:i])) == 4:
+        break
+
+print("Pt 1:", i) # 1896
+
+for i, char in enumerate(signal):
+    if i <= 12:
+        continue
+    if len(set(signal[i-14:i])) == 14:
+        break
+    
+print("Pt 2:", i) # 3452
