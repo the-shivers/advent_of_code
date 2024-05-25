a1de35c54867a681c1d8e397cf167542215477df
the_shivers <rossbrian120@gmail.com>
2024-05-25 18:37:39 -0500

    Learned about dynamic programming :D

--- 2023/day12/solution.py
+++ 2023/day12/solution.py
@@ -54,45 +54,33 @@ def verbose_solve(filename):
         possibilities = generate_brute_force_list(string, int_list)
         solutions = get_solutions(possibilities, int_list)
         pt_1_answer += len(solutions)
-        print(f'string {string} with int_list {int_list} had {len(solutions)} solutions.\n{solutions}\n')
+        # print(f'string {string} with int_list {int_list} had {len(solutions)} solutions.\n{solutions}\n')
     return pt_1_answer
 
 print(verbose_solve('input.txt'))
 
 # Part 2
 
-# I may have made a huge mistake. Let's just roll with it though and see what happens.
+# I may have made a huge mistake trying brute force. Let's just roll with it though and see what happens.
 
-def unfold(string, int_list):
+def unfold(string, int_tup):
+    """We need int_tup to be a tuple, so that its hashable for our recursive solution."""
     string = (string + '?') * 5
     string = string[:-1]
-    int_list = int_list * 5
-    return string, int_list
-
-def verbose_solve2(filename):
-    puzzle=get_puzzle(filename)
-    pt_1_answer = 0
-    for string, int_list in puzzle:
-        string, int_list = unfold(string, int_list)
-        possibilities = generate_brute_force_list(string, int_list)
-        solutions = get_solutions(possibilities, int_list)
-        pt_1_answer += len(solutions)
-        print(f'string {string} with int_list {int_list} had {len(solutions)} solutions.\n{solutions}\n')
-    return pt_1_answer
-
-print(verbose_solve2('example.txt')) # Times out. :(
+    int_tup = int_tup * 5
+    return string, tuple(int_tup)
 
 # I am thinking of recursion with hash tables. I'm likely to encounter the 
 # same substring / int groups # many times and I can save time by stashing 
 # the solution to them.
 
-filename = 'input.txt'
-puzzle = get_puzzle(filename)
-string, int_list = unfold(*puzzle[1])
-question_indices = [i for i, ltr in enumerate(string) if ltr == '?']
-q = len(question_indices)
-current_broken_parts = string.count('#')
-total_broken_parts = sum(int_list)
+# filename = 'input.txt'
+# puzzle = get_puzzle(filename)
+# string, int_tup = unfold(*puzzle[1])
+# question_indices = [i for i, ltr in enumerate(string) if ltr == '?']
+# q = len(question_indices)
+# current_broken_parts = string.count('#')
+# total_broken_parts = sum(int_tup)
 # So total possibilities = total_broken_parts - current_broken_parts choose q e.g. 92k.
 
 # '?###??????????###??????????###??????????###??????????###????????'
@@ -103,13 +91,52 @@ total_broken_parts = sum(int_list)
 # [1, 2, 2, 3, 2, 1, 2, 2, 3, 2, 1, 2, 2, 3, 2, 1, 2, 2, 3, 2, 1, 2, 2, 3, 2]
 # Answer: unknown
 # Possibilities: 69 choose 40
-# Over quintillion combinations lmao
+# Over quintillion combinations lmaooooo
 
+# Alright, I am throwing in the towel and looking at solutions others have developed.
+# This superb writeup by @xavdid basically confirmed the approach I was going to go with
+# (recursive + hashed), but helped me get unstuck. Apparently this is called "dynamic programming"
+# which somehow I didn't know about, outside of toy examples e.g. fibonacci.
+# https://advent-of-code.xavd.id/writeups/2023/day/12/
 
+from functools import cache
 
+@cache
+def get_valid_solutions_count(string: str, int_tup: list[int]) -> int:
+    # define base case
+    if not string: # string == ''
+        return len(int_tup) == 0 # bool, i.e. 1
+        # since if no string left, we only succeed if no groups left
+    if not int_tup: # int_tup == []
+        return '#' not in string # bool, i.e. 1, 
+        # since if no broken part groups remain in int_tup
+        # we only get a success if remaining list is all '?' and '.'
+    # Reduce remaining cases toward base case
+    char, rem_string = string[0], string[1:]
+    if char == '.':
+        return get_valid_solutions_count(rem_string, int_tup)
+    if char == '#':
+        group = int_tup[0]
+        # We're in the first grouping. It has to have exactly the right number of '#' and '?' to succeed.
+        if (
+            len(string) >= group # string must be long enough for whole group
+            and all(i != '.' for i in string[:group]) # string must not contain '.'
+            and (len(string) == group or string[group] != "#") # character after full 
+            # group must not also be #, which indicates a failure (groups cannot touch)
+        ):
+            return get_valid_solutions_count(string[group + 1:], int_tup[1:])
+        return 0 # failure if above requirements aren't met
+    if char == '?': # sum case if . and case if #
+        solutions_if_period = get_valid_solutions_count(f".{rem_string}", int_tup)
+        solutions_if_pound = get_valid_solutions_count(f"#{rem_string}", int_tup)
+        return solutions_if_period + solutions_if_pound
+    
+def solve_pt_2(filename):
+    puzzle = get_puzzle(filename)
+    total = 0
+    for string, int_tup in puzzle:
+        string, int_tup = unfold(string, int_tup)
+        total += get_valid_solutions_count(string, int_tup)
+    return total
 
-
-
-
-
-
+print(solve_pt_2('input.txt')) # 8475948826693
