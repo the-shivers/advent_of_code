c68d95543f3cd18b7ea40252c36e2bfe7131ca53
the-shivers <rossbrian120@gmail.com>
2024-12-02 12:33:47 -0600

    Improved day 2 solution.

--- 2024/day2/solution.py
+++ 2024/day2/solution.py
@@ -1,45 +1,26 @@
-"""
-Notes:
-As I get more experienced, I wonder if maybe people worship DRY code too much.
-A lot of the time, having duplicated code makes things more readable!
-
-Here, it would certainly be straightforward to combine is_increasing_report
-and is_decreasing_report into a single function. It might be more performant
-as well. But to what end? This code is so readable and intuitive, and this isn't
-a problem that demands high performance.
-"""
-
 # input_txt = 'advent_of_code/2024/day2/example.txt'
 input_txt = 'advent_of_code/2024/day2/input.txt'
 with open(input_txt) as file:
     lines = [[int(i) for i in line.strip().split()] for line in file]
 
 # Part 1
-def is_increasing_report(report: list[int]) -> bool:
-    """Returns true if report is valid and increasing"""
+def is_report_valid_increasing(report: list[int]) -> bool:
     for i in range(len(report) - 1):
         difference = report[i + 1] - report[i]
         if difference < 1 or difference > 3:
             return False
     return True
 
-def is_decreasing_report(report: list[int]) -> bool:
-    """Returns true if report is valid and decreasing"""
-    for i in range(len(report) - 1):
-        difference = report[i] - report[i + 1]
-        if difference < 1 or difference > 3:
-            return False
-    return True
-
-def evaluate_report(report: list[int]) -> bool:
-    """Returns true if report is valid."""
-    assert len(report) > 1, "Report length must be greater than 1."
-    return is_increasing_report(report) or is_decreasing_report(report)
+def is_report_valid(report: list[int]) -> bool:
+    return (
+        is_report_valid_increasing(report) or 
+        is_report_valid_increasing(report[::-1])
+    )
 
 total_safe = 0
 for report in lines:
-    total_safe += 1 if evaluate_report(report) else 0
-print(total_safe)
+    total_safe += 1 if is_report_valid(report) else 0
+print(f'Part 1: {total_safe}')
 
 # Part 2
 def get_report_variations(report: list[int]) -> list[list[int]]:
@@ -51,9 +32,8 @@ def get_report_variations(report: list[int]) -> list[list[int]]:
 
 total_safe = 0
 for report in lines:
-    report_variations = get_report_variations(report)
-    for v in report_variations:
-        if evaluate_report(v):
+    for v in get_report_variations(report):
+        if is_report_valid(v):
             total_safe += 1
             break
-print(total_safe)
+print(f'Part 2: {total_safe}')
