37242053accfcc051971a5d721610c90fa5467c8
the_shivers <rossbrian120@gmail.com>
2024-12-03 00:24:55 -0600

    Improved day 3.

--- 2024/day3/solution.py
+++ 2024/day3/solution.py
@@ -7,28 +7,10 @@ with open(input_txt) as file:
 
 # Part 1
 def get_products(s: str) -> int:
-    regex = re.compile(r'mul\((\d+),(\d+)\)')
-    matches = regex.findall(s)
-    sum = 0
-    for pair in matches:
-        sum += int(pair[0]) * int(pair[1])
-    return sum
-
+    matches = re.findall(r'mul\((\d+),(\d+)\)', s)
+    return sum([int(pair[0]) * int(pair[1]) for pair in matches])
 print(f'Part 1: {get_products(input_str)}')
 
 # Part 2
-total = 0
-remaining = input_str[:]
-while True:
-    parts_list = remaining.split("don't()", 1)
-    if len(parts_list) == 1:
-        total += get_products(parts_list[0])
-        break
-    good, bad = parts_list
-    total += get_products(good)
-    parts_list = bad.split("do()", 1)
-    if len(parts_list) == 1:
-        break
-    bad, remaining = parts_list
-
-print(f'Part 2: {total}')
+substrs = re.findall(r'^.*?don\'t\(\)|do\(\).*?(?:don\'t\(\)|$)', input_str)
+print(f'Part 2: {sum([get_products(s) for s in substrs])}')
