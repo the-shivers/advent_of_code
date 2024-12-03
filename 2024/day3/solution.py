fccdfa6a698fd6beb74f579f6968cd2c2b85486b
the_shivers <rossbrian120@gmail.com>
2024-12-03 00:32:08 -0600

    Further improved day3.

--- 2024/day3/solution.py
+++ 2024/day3/solution.py
@@ -3,14 +3,16 @@ import re
 # input_txt = 'advent_of_code/2024/day3/example.txt'
 input_txt = 'advent_of_code/2024/day3/input.txt'
 with open(input_txt) as file:
-    input_str = ''.join([line.strip() for line in file])
+    input_str = ''.join(line.strip() for line in file)
 
 # Part 1
 def get_products(s: str) -> int:
     matches = re.findall(r'mul\((\d+),(\d+)\)', s)
-    return sum([int(pair[0]) * int(pair[1]) for pair in matches])
+    return sum(int(pair[0]) * int(pair[1]) for pair in matches)
+
 print(f'Part 1: {get_products(input_str)}')
 
 # Part 2
 substrs = re.findall(r'^.*?don\'t\(\)|do\(\).*?(?:don\'t\(\)|$)', input_str)
-print(f'Part 2: {sum([get_products(s) for s in substrs])}')
+
+print(f'Part 2: {sum(get_products(s) for s in substrs)}')
