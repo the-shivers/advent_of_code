84bad1691da9c6e130142fa8f6b72d5499926aae
the-shivers <rossbrian120@gmail.com>
2024-12-05 03:22:52 -0600

    Mega clean day 5.

--- 2024/day5/solution.py
+++ 2024/day5/solution.py
@@ -1,63 +1,32 @@
-# input_txt = 'advent_of_code/2024/day5/example.txt'
-input_txt = 'advent_of_code/2024/day5/input.txt'
-rules = []
-updates = []
-with open(input_txt) as file:
-    for line in file:
-        if '|' in line.strip():
-            rule = [int(i) for i in line.strip().split('|')]
-            rules.append(rule)
-        elif ',' in line.strip():
-            update = [int(i) for i in line.strip().split(',')]
-            updates.append(update)
+with open('advent_of_code/2024/day5/input.txt') as file:
+    rules, updates = file.read().split('\n\n')
+rules = [list(map(int, r.split('|'))) for r in rules.split('\n')]
+updates = [list(map(int, u.split(','))) for u in updates.split('\n')]
 
-def get_middle_page_number(update: list[int]) -> int:
-    return update[len(update) // 2]
-
-def test_update(update: list[int], rules: list[list[int]]) -> bool:
-    for rule in rules:
-        if rule[0] in update and rule[1] in update:
-            if update.index(rule[0]) > update.index(rule[1]):
-                return False
-        else:
-            continue
-    return True
-
-# Part 1
-total = 0
-for update in updates:
-    if test_update(update, rules):
-        total += get_middle_page_number(update)
-print(total)
-
-# Part 2
-def get_loss_dict(rules: list[list[int]]) -> dict[int, int]:
-    dict = {}
-    for rule in rules:
-        if rule[0] in dict:
-            dict[rule[0]] += 1
-        else:
-            dict[rule[0]] = 1
-    return dict
-
-def order_update(update: list[int], rules: list[list[int]]) -> list[int]:
-    relevant_rules = []
-    for rule in rules:
-        if rule[0] in update and rule[1] in update:
-            relevant_rules.append(rule)
-    ordered = [0] * len(update)
-    loss_dict = get_loss_dict(relevant_rules)
-    sorted_keys = sorted(loss_dict.keys(), key=loss_dict.get, reverse=True)
+def order_pages(update: list[int], rules: list[list[int]]) -> list[int]:
+    """
+    Since relevant rules always give a comparison of every page to every other
+    page, we can just count unique pages coming before a given page--this will
+    be the index in the correctly sorted list!
+    """
+    relevant_rules = {}
+    sorted_nums = [0] * len(update)
+    for src, dst in rules:
+        if src in update and dst in update:
+            if dst not in relevant_rules:
+                relevant_rules[dst] = 0
+            relevant_rules[dst] += 1
     for page in update:
-        if page in sorted_keys:
-            ordered[sorted_keys.index(page)] = page
-        else:
-            ordered[-1] = page
-    return ordered
+        sorted_nums[relevant_rules.get(page, 0)] = page
+    return sorted_nums
 
-total = 0
+pt1, pt2 = 0, 0
 for update in updates:
-    if not test_update(update, rules):
-        ordered = order_update(update, rules)
-        total += get_middle_page_number(ordered)
-print(total)
+    sorted_update = order_pages(update, rules)
+    if update == sorted_update:
+        pt1 += update[len(update) // 2]
+    else:
+        pt2 += sorted_update[len(sorted_update) // 2]
+
+print("Part 1:", pt1)
+print("Part 2:", pt2)
