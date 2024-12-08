9376ae76028028e0371a736c597e631c88adc277
the_shivers <rossbrian120@gmail.com>
2024-12-07 22:50:35 -0600

    Clean d7 solution.

--- 2024/day7/solution.py
+++ 2024/day7/solution.py
@@ -1,4 +1,3 @@
-# input_txt = 'example.txt'
 input_txt = 'input.txt'
 with open(input_txt) as file:
     answers = []
@@ -7,55 +6,22 @@ with open(input_txt) as file:
         nums = [int(num) for num in r.strip().split()]
         answers.append((int(l.strip()), tuple(nums)))
 
-def get_bin_list(num: int) -> list[int]:
-    binary_str = str(bin(num)).split('0b')[1]
-    return [int(i) for i in binary_str]
+def add(n1, n2): return n1 + n2
+def mult(n1, n2): return n1 * n2
+def concat(n1, n2): return int(str(n1) + str(n2))
 
-def get_tri_list(num: int) -> list[int]:
-    if num == 0:
-        return [0]
-    nums = []
-    while num:
-        num, r = divmod(num, 3)
-        nums.append(r)
-    return nums[::-1]
+def recurse(target, total, nums):
+    if len(nums) == 0:
+        return total == target
+    elif total > target:
+        return False
+    else:
+        return any([recurse(target, f(total, nums[0]), nums[1:]) for f in funcs])
 
-def get_sign_permutations(i, base):
-    keys = '+*|'
-    perms = {0: []}
-    for i in range(1, i):
-        perms[i] = []
-        for j in range(base**i): # generation
-            bin_list = get_bin_list(j) if base == 2 else get_tri_list(j)
-            indices = [0] * (i - len(bin_list)) + bin_list
-            perms[len(indices)].append([keys[i] for i in indices])
-    return perms
+funcs = (add, mult)
+print('Part 1:', sum(a[0] if recurse(a[0], a[1][0], a[1][1:]) else 0 for a in answers))
 
-def evaluate_line(nums, signs):
-    total = nums[0]
-    for i, sign in enumerate(signs):
-        if sign == '+':
-            total += nums[i + 1]
-        elif sign == '|':
-            total = int(str(total) + str(nums[i + 1]))
-        else:
-            total *= nums[i + 1]
-    return total
-
-def solve(answers, perms):
-    total = 0
-    for answer, nums in answers:
-        for perm in perms[len(nums) - 1]:
-            if evaluate_line(nums, perm) == answer:
-                total += answer
-                break
-    return total
+funcs = (add, mult, concat)
+print('Part 2:', sum(a[0] if recurse(a[0], a[1][0], a[1][1:]) else 0 for a in answers))
 
 
-# Part 1
-perms = get_sign_permutations(12, 2)
-print("Part 1:", solve(answers, perms))
-
-# Part 2:
-perms = get_sign_permutations(12, 3)
-print("Part 2:", solve(answers, perms))
