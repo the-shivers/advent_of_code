00488a5958d124eed7136cede3a484c0061429c0
the-shivers <rossbrian120@gmail.com>
2024-12-08 16:49:12 -0600

    a'

--- 2024/day7/solution.py
+++ 2024/day7/solution.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     pt1 = pt2 = 0
     ops1 = (lambda x, y: x + y, lambda x, y: x * y)  # +, *
     ops2 = (*ops1, lambda x, y: int(str(x) + str(y)))  # +, *, ||
+
     with open('input.txt') as file:
         while line := file.readline():
             target_str, numbers_str = line.strip().split(': ')
@@ -19,4 +20,5 @@ if __name__ == '__main__':
             nums = [int(n) for n in numbers_str.split()]
             pt1 += target if recurse(target, nums[0], nums[1:], ops1) else 0
             pt2 += target if recurse(target, nums[0], nums[1:], ops2) else 0
+            
     print(f'Part 1: {pt1}\nPart 2: {pt2}')
