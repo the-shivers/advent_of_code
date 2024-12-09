270dea8d36bf9599d2535b30e8d5e46e213fad98
the_shivers <rossbrian120@gmail.com>
2024-12-09 12:14:34 -0600

    Day 9 optimized.

--- 2024/day9/solution.py
+++ 2024/day9/solution.py
@@ -40,48 +40,54 @@ def checksum(s_contract):
             checksum += i['pos'] * i['id']
     return checksum
 
-def contract_pt2(s_expand):
-    l1, l2, r1, r2 = 0, 1, len(s_expand) - 1, len(s_expand) - 1
-    needed = 0
-    free_space = 0
-    while r1 > 0:
-        if r1 % 500 == 0:
-            print(r1)
-        if s_expand[r2]['id'] is None:
-            r2 -= 1
-            r1 -= 1
-            continue
-        current_id = s_expand[r2]['id']
-        while r1 - 1 >= 0 and s_expand[r1 - 1]['id'] == current_id:
-            r1 -= 1
-        needed = r2 - r1 + 1
-        free_space = 0
-        l1, l2 = 0, 0
-        while l2 < r1:
-            if l1 < len(s_expand) and s_expand[l1]['id'] is not None:
-                l1 += 1
-                l2 += 1
-                continue
-            while l2 + 1 < len(s_expand) and s_expand[l2 + 1]['id'] is None:
-                l2 += 1
-            free_space = l2 - l1 + 1
-            if free_space >= needed:
-                for i in range(needed):
-                    s_expand[l1 + i]['id'], s_expand[r2 - i]['id'] = s_expand[r2 - i]['id'], s_expand[l1 + i]['id']
-                l1, l2 = l1 + needed, l1 + needed
+def expand2(s):
+    is_file = True
+    files, spaces = [], []
+    pos, id = 0, 0
+    for i in s:
+        if is_file:
+            if int(i) > 0:  # Only create file entry if length > 0
+                files.append({"pos": pos, "id": id, "len": int(i)})
+                id += 1
+            pos += int(i)
+            is_file = False
+        else:
+            if int(i) > 0:  # Only create space entry if length > 0
+                spaces.append({"pos": pos, "id": None, "len": int(i)})
+            pos += int(i)
+            is_file = True
+    return files, spaces
+
+def contract2(files, spaces):
+    for file in reversed(files):
+        for space in spaces:
+            if space['len'] >= file['len'] and space['pos'] < file['pos']:
+                if 'content' not in space:
+                    space['content'] = []
+                space['content'].append({"id": file['id'], 'len': file['len']})
+                space['len'] -= file['len']
+                file['id'] = None
                 break
-            else:
-                l1, l2 = l2 + 1, l2 + 1
-        r2 = r1 - 1
-        r1 = r1 - 1 
-        needed = 0
-        current_id = None
-    return s_expand
-        
+    return files, spaces
+
+def checksum2(files, spaces):
+    total = 0
+    for f in files:
+        if f['id'] is not None:
+            total += sum(f['id'] * (f['pos'] + i) for i in range(f['len']))
+    for s in spaces:
+        if 'content' in s:
+            offset = 0
+            for f in s['content']:
+                for i in range(f['len']):
+                    total += f['id'] * (s['pos'] + offset)
+                    offset += 1
+    return total
+
 s_expand = expand(line)
 s_contract = contract(s_expand)
 print("Part 1:", checksum(s_contract)) # 6346871685398
 
-s_expand = expand(line)
-s_contract2 = contract_pt2(s_expand) 
-print("Part 2:", checksum(s_contract2)) # 6373055193464
+files, spaces = expand2(line)
+files, spaces = contract2(files, spaces)
+print("Part 2:", checksum2(files, spaces)) # 6373055193464
