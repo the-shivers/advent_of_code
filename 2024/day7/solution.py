bdf384400fdbc500fdd09b89010968f9f910c74a
the-shivers <rossbrian120@gmail.com>
2024-12-10 04:44:10 -0600

    Improved performance by working backwards.

--- 2024/day7/solution.py
+++ 2024/day7/solution.py
@@ -1,24 +1,33 @@
-def recurse(target, total, nums, ops):
-    if len(nums) == 0: 
-        return total == target
-    elif total > target: 
-        return False
-    else: 
-        return any(
-            [recurse(target, f(total, nums[0]), nums[1:], ops) for f in ops]
-        )
+def validate(target, nums, allow_concat=False):
+    if len(nums) == 1: return target == nums[0]
+    if nums[-1] > target: return False
+    # Multiplication Case
+    if target % nums[-1] == 0:
+        if validate(target // nums[-1], nums[:-1], allow_concat):
+            return True
+    # Concatenation Case
+    if allow_concat:
+        digit_count = len(str(nums[-1]))
+        if target % (10 ** digit_count) == nums[-1]:
+            if validate(target // (10 ** digit_count), nums[:-1], allow_concat):
+                return True
+    # Addition Case
+    return validate(target - nums[-1], nums[:-1], allow_concat)
 
-if __name__ == '__main__':
-    pt1 = pt2 = 0
-    ops1 = (lambda x, y: x + y, lambda x, y: x * y)  # +, *
-    ops2 = (*ops1, lambda x, y: int(str(x) + str(y)))  # +, *, ||
-
-    with open('input.txt') as file:
-        while line := file.readline():
+def solve_puzzle(filename: str) -> tuple[int, int]:
+    part1 = part2 = 0
+    with open(filename) as file:
+        for line in file:
             target_str, numbers_str = line.strip().split(': ')
             target = int(target_str)
             nums = [int(n) for n in numbers_str.split()]
-            pt1 += target if recurse(target, nums[0], nums[1:], ops1) else 0
-            pt2 += target if recurse(target, nums[0], nums[1:], ops2) else 0
-            
-    print(f'Part 1: {pt1}\nPart 2: {pt2}')
+            if validate(target, nums, False):
+                part1 += target
+            if validate(target, nums, True):
+                part2 += target
+    return part1, part2
+
+if __name__ == '__main__':
+    pt1, pt2 = solve_puzzle('input.txt')
+    print(f'Part 1: {pt1}')
+    print(f'Part 2: {pt2}')
