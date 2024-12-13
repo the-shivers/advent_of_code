f81ffc5e8b2d4aabf152d64c1869575aafb024fd
the-shivers <rossbrian120@gmail.com>
2024-12-13 01:06:07 -0600

    Washed.

--- 2024/day13/solution.py
+++ 2024/day13/solution.py
@@ -19,22 +19,20 @@ def get_coefficients(v1, v2, target):
     """Cramer's rule. Checks if vals close to integer before returning. 
     Input never has a zero determinant, so we don't worry about it."""
     det = v1[0] * v2[1] - v1[1] * v2[0]
-    a = (target[0] * v2[1] - target[1] * v2[0]) / det
-    b = (v1[0] * target[1] - v1[1] * target[0]) / det
-    if abs(a - round(a)) < 1e-10 and abs(b - round(b)) < 1e-10:
-        return round(a), round(b)
-    return None
+    a = (target[0] * v2[1] - target[1] * v2[0])
+    b = (v1[0] * target[1] - v1[1] * target[0])
+    if a % det or b % det:
+        return (0, 0) # if non-integer answers
+    return (a // det, b // det)
 
 def solve(filename):
     games = get_games(filename)
     pt1 = pt2 = 0
     for game in games:
         result1 = get_coefficients(game['A'], game['B'], game['P'])
-        if result1:
-            pt1 += 3 * result1[0] + 1 * result1[1]
+        pt1 += 3 * result1[0] + 1 * result1[1]
         result2 = get_coefficients(game['A'], game['B'], game['P2'])
-        if result2:
-            pt2 += 3 * result2[0] + 1 * result2[1]
+        pt2 += 3 * result2[0] + 1 * result2[1]
     print(f"Part 1: {pt1}\nPart 2: {pt2}")
           
 if __name__ == "__main__":
