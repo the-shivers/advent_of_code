ac14bbfbd36cbeb58e5ff19042bed069ee8f468a
the-shivers <rossbrian120@gmail.com>
2024-12-24 02:27:35 -0600

    Clean

--- 2024/day24/solution.py
+++ 2024/day24/solution.py
@@ -1,30 +1,15 @@
 from collections import deque
 
-inputs = {}
-combos = deque()
-input_txt = 'input.txt'
-with open(input_txt) as file:
-    for line in file:
-        if not line.strip():
-            continue
-        if ':' in line:
-            k, v = line.strip().split(': ')
-            inputs[k] = int(v)
-        else:
-            combos.append([i for i in line.strip().split(' ') if i != '->'])
+with open('input.txt') as f:
+    vals, ops = f.read().strip().split('\n\n')
+inputs = {k: int(v) for k, v in (l.split(': ') for l in vals.splitlines())}
+combos = deque([[j for j in i.split() if j != '->'] for i in ops.splitlines()])
 
 def swap_outputs(combos, o1, o2):
-    for combo1 in combos:
-        if combo1[3] == o1:
-            for combo2 in combos:
-                if combo2[3] == o2:
-                    combo1[3], combo2[3] = o2, o1
-                    return combos
-        elif combo1[3] == o2:
-            for combo2 in combos:
-                if combo2[3] == o1:
-                    combo1[3], combo2[3] = o1, o2
-                    return combos
+    idx1 = next((i for i, combo in enumerate(combos) if combo[3] == o1))
+    idx2 = next((i for i, combo in enumerate(combos) if combo[3] == o2))
+    combos[idx1][3], combos[idx2][3] = combos[idx2][3], combos[idx1][3]
+    return combos
 
 def evaluate(input1, operator, input2):
     if operator == 'AND': return input1 and input2
@@ -33,28 +18,32 @@ def evaluate(input1, operator, input2):
 
 def fill_inputs(inputs, combos):
     while combos:
-        curr = combos.pop()
+        curr = combos.popleft()
         if curr[0] in inputs and curr[2] in inputs:
             inputs[curr[3]] = evaluate(inputs[curr[0]], curr[1], inputs[curr[2]])
         else:
-            combos.appendleft(curr)
+            combos.append(curr)
     return inputs
 
 new_inputs = fill_inputs(inputs.copy(), combos.copy())
-print("Part 1:", int(''.join(str(new_inputs[k]) for k in sorted((k for k in new_inputs if k.startswith('z')), reverse=True)), 2))
+z_outputs = {k: v for k, v in new_inputs.items() if k.startswith('z')}
+binary = ''.join(str(z_outputs[k]) for k in sorted(z_outputs, reverse=True))
+print("Part 1:", int(binary, 2))
 
 # Part 2
 # Solved manually by examining input ¯\_(ツ)_/¯
-combos = swap_outputs(combos, 'kth', 'z12')
-combos = swap_outputs(combos, 'gsd', 'z26')
-combos = swap_outputs(combos, 'z32', 'tbt')
-combos = swap_outputs(combos, 'qnf', 'vpm')
+swaps = [('kth', 'z12'), ('gsd', 'z26'), ('z32', 'tbt'), ('qnf', 'vpm')]
+for o1, o2 in swaps:
+    combos = swap_outputs(combos, o1, o2)
 new_inputs = fill_inputs(inputs, combos)
 
-x_str = ''.join([str(new_inputs[k]) for k in sorted((k for k in new_inputs if k.startswith('x')), reverse=True)])
-y_str = ''.join([str(new_inputs[k]) for k in sorted((k for k in new_inputs if k.startswith('y')), reverse=True)])
-target = int(x_str, 2) + int(y_str, 2)
-z_str = ''.join([str(new_inputs[k]) for k in sorted((k for k in new_inputs if k.startswith('z')), reverse=True)])
-result = int(z_str, 2)
+x_outputs = {k: v for k, v in new_inputs.items() if k.startswith('x')}
+y_outputs = {k: v for k, v in new_inputs.items() if k.startswith('y')}
+z_outputs = {k: v for k, v in new_inputs.items() if k.startswith('z')}
+
+target = int(''.join(str(x_outputs[k]) for k in sorted(x_outputs, reverse=True)), 2) + \
+         int(''.join(str(y_outputs[k]) for k in sorted(y_outputs, reverse=True)), 2)
+result = int(''.join(str(z_outputs[k]) for k in sorted(z_outputs, reverse=True)), 2)
+
 if result == target:
-    print("Part 2:", ",".join(sorted(['kth', 'z12', 'gsd', 'z26', 'z32', 'tbt', 'qnf', 'vpm'])))
+    print("Part 2:", ",".join(sorted(sum(swaps, ()))))
